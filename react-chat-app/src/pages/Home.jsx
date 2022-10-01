134adb11cc502e8fce19e94a5457ecb5ed4829e4
hungngo04 <ngotienhung195@gmail.com>
2022-10-01 17:02:40 -0400

    add components home page

--- react-chat-app/src/pages/Home.jsx
+++ react-chat-app/src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Sidebar } from '../components/Sidebar'
+import { Chat } from '../components/Chat'
 
-export const Home = () => {
-  return (
-    <div>Home</div>
-  )
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="container">
+                <Sidebar></Sidebar>
+                <Chat></Chat>
+            </div>
+        </div>
+    )
 }
+
+export default Home
