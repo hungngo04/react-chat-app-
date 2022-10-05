1f9143f61df83e1120faa4654eeb8d67ddb21c9c
hungngo04 <ngotienhung195@gmail.com>
2022-10-04 22:42:52 -0400

    add chat components

--- react-chat-app/src/components/Chat.jsx
+++ react-chat-app/src/components/Chat.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import '../style.scss'
-import cam from '../img/cam.png'
-import add from '../img/add.png'
-import more from '../img/more.png'
-
+import Messages from './Messages'
+import Input from './Input'
 
 export const Chat = () => {
   return (
@@ -18,8 +16,10 @@ export const Chat = () => {
           <span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none" stroke="#878A92" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg></span>
           <span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#878A92" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg></span>
           <span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#878A92" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg></span>
-        </div>
+        </div>        
       </div>
+      <Messages />
+      <Input />
     </div>
   )
 }
---
+++ react-chat-app/src/components/Input.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Input = () => {
+  return (
+    <div className="input">
+        Input
+    </div>
+  )
+}
+
+export default Input
